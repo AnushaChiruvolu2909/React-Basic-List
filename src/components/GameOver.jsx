fb67443abdfd24f72386094ef0c453b36a7c5578
ANUSHA CHIRUVOLU <Siddhartha.Gumparla@cibc.com>
2024-05-30 19:01:05 -0400

    second commit

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 export default function GameOver({ winner,onRematch }) {
   return (
     <div id="game-over">
-      <h2>Game Over</h2>
+      <h2>Game Overr</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p> Match Drawn</p>}
       <p>
